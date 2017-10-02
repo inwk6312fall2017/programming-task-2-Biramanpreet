0dd491138ad1d5e9dadee83160d0f3414c502bfa
inwkstudent <biramanpreet.singh@dal.ca>
2017-10-02 12:57:24 -0300

    storing the output in new file

--- task.py
+++ task.py
@@ -4,7 +4,8 @@ with open('Crime.csv',"r") as file:
     contents = file.read() 
     return (contents) 
 
+new_file=open((("inwkstudent@UbuntuMain:~/Documents/Quizz/programming-task-2-Biramanpreet")+ 'new_file'),'w')
+
 def parsing(file):
    for EVT_DATE in file:
-      match 
-
+      match
